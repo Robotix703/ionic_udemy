fa4d9738dff28474eb121e7f94a85bf362d28a94
Robotix703 <stephane.chatelle@orange.fr>
2022-08-14 21:35:44 +0200

    Update recipe page

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: "&",
+      title: "Choucroute",
+      imageUrl: "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal#/media/Fichier:03GustavoPetro.jpg",
+      ingredients: [
+        "saucisses",
+        "chou"
+      ]
+    },
+    {
+      id: "&",
+      title: "Blanquette",
+      imageUrl: "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal#/media/Fichier:Space_Shuttle_Columbia_launching.jpg",
+      ingredients: [
+        "sauce",
+        "saucisses"
+      ]
+    }
+  ]
 
   constructor() { }
 
