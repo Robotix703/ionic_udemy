417e1f066dc9f7408948c7e0da512f55b5e6fc33
Robotix703 <stephane.chatelle@orange.fr>
2022-10-17 13:25:43 +0200

    Add new structure

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Ionic project made from udemy course
 
   ionic generate : Pour générer un nouveau module
 
+  ionic generate page <NOM> : Générer une nouvelle page
+
   ng serve : Lance l'application en local
 
 # Liens utiles
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,16 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'recipes',
-    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
   },
 ];
 
