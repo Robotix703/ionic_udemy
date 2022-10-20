d7750abf9b27ee7c10354a82ffa9a28854d9c4e3
Robotix703 <stephane.chatelle@orange.fr>
2022-10-20 22:58:57 +0200

    Add booking places/navigation

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Place } from '../../places.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferBookingsPage implements OnInit {
 
-  constructor() { }
+  place: Place;
+
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack("/places/offers");
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
 }
